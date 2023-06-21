60e47b8d692af20b94eea5ceb262507678d07ba2
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2023-06-21 20:28:47 +0800

    eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import {FC} from 'react';
+import { FC } from 'react';
 
 import './App.css';
 
 export const App: FC = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};

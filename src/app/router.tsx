3ff8c30bbd718c800cdf1da0a5faa36aad75bba9
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2023-07-11 21:11:33 +0800

    navigate to root if error route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,16 +1,15 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 import { Root } from './routes/root/Root';
 import { Experience } from './routes/experience/Experience';
 import { Education } from './routes/education/Education';
 import { Portfolio } from './routes/portfolio/Portfolio';
-import { ErrorPage } from './routes/error-page/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    errorElement: <ErrorPage />,
+    errorElement: <Navigate to='/' replace={true} />,
     children: [
       { index: true, element: <Experience /> },
       { path: 'edu', element: <Education /> },

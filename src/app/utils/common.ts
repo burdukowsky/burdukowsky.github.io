549ee4e5b7c309146a39a7394013f252137ed3c3
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2023-07-11 19:06:28 +0800

    replace theme class to "html" tag

--- src/app/utils/common.ts
+++ src/app/utils/common.ts
@@ -3,7 +3,7 @@ import { getLocalStorageValue } from './local-storage';
 import { defaultTheme, lsThemeKey } from '../globals';
 
 export function setColorTheme(theme: Theme): void {
-  document.body.className = theme === 'default' ? '' : theme;
+  document.documentElement.className = theme === 'default' ? '' : theme;
 }
 
 export function configureInitialTheme(): void {

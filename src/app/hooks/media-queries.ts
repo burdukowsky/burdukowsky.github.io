ee98ff31fe2422f8e2ad92215faec375efaf28c2
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2023-07-13 08:46:48 +0800

    adapt Hello for mobile

--- src/app/hooks/media-queries.ts
+++ src/app/hooks/media-queries.ts
@@ -3,3 +3,7 @@ import { useMediaQuery } from 'react-responsive';
 export function useMobileMediaQuery(): boolean {
   return useMediaQuery({ maxWidth: 767 });
 }
+
+export function useExtraMobileMediaQuery(): boolean {
+  return useMediaQuery({ maxWidth: 575 });
+}
--- src/app/layout/header/components/hello/Hello.tsx
+++ src/app/layout/header/components/hello/Hello.tsx
@@ -1,27 +1,17 @@
 import { FC } from 'react';
 
 import { Space } from 'app/components/space/Space';
-import { AppText } from 'app/components/app-text/AppText';
 import styles from './Hello.module.scss';
 import { Contacts } from '../contacts/Contacts';
 
 export const Hello: FC = () => {
   return (
-    <Space
-      direction='column'
-      height='100%'
-      justifyContent='center'
-      className={styles.Hello}
-    >
-      <AppText as='p' color='primary' size='1.8rem'>
-        Hi, I'm
-      </AppText>
-      <AppText as='h1' size='2.8rem'>
-        Stanislav Burdukowsky
-      </AppText>
-      <AppText as='p' size='1.7rem'>
-        Senior Frontend Developer
-      </AppText>
+    <Space direction='column' height='100%' justifyContent='center' gap='.5em'>
+      <Space direction='column'>
+        <p className={styles.HelloHi}>Hi, I'm</p>
+        <h1 className={styles.HelloName}>Stanislav Burdukowsky</h1>
+        <p className={styles.HelloSenior}>Senior Frontend Developer</p>
+      </Space>
       <Contacts />
     </Space>
   );

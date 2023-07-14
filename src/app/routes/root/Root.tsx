f86f0a12e9d5f213c3d5b582a46d964405ec5e5d
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2023-07-14 11:48:28 +0800

    auto scroll to top

--- src/app/routes/root/Root.tsx
+++ src/app/routes/root/Root.tsx
@@ -3,8 +3,10 @@ import { FC } from 'react';
 import { useMobileMediaQuery } from '../../hooks/media-queries';
 import { RootDefault } from './components/root-default/RootDefault';
 import { RootMobile } from './components/root-mobile/RootMobile';
+import { useAutoScrollToTop } from '../../hooks/useAutoScrollToTop';
 
 export const Root: FC = () => {
   const mobile = useMobileMediaQuery();
+  useAutoScrollToTop();
   return <div id='rootChild'>{mobile ? <RootMobile /> : <RootDefault />}</div>;
 };
